cca3aa66e93d675b07f785efbcfbbf3003343ef1
Lucas Alves <lucasdeveloper07@gmail.com>
2026-03-25 00:44:18 -0300

    Add project files.

---
+++ WebApplicationAPI/WebApplicationAPI/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPI.DTO;
+
+namespace WebApplicationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest("ID inválido");
+            }
+
+            var user = new { Id = id, Name = "Lucas Alves", Email = "lucas@gmail.com"};
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = new[]
+            {
+                new { Id = 1, Name = "Lucas", Email = "lucas@escanor.com"},
+                new { Id = 2, Name = "Alves", Email = "alves@escanor.com"}
+            };
+
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] UserDTO user)
+        {
+            if (string.IsNullOrEmpty(user.Name))
+            {
+                return BadRequest("Faltou o nome aqui");
+            }
+
+            var createdUser = new { Id = 3, Name = user.Name, Email = user.Email };
+
+            return Created($"/User/{createdUser.Id}", createdUser);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] UserDTO updatedUser)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("ID inválido");
+            }
+
+            return Ok(new { Id = id, Name = updatedUser.Name, Email = updatedUser.Email });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("ID inválido");
+            }
+
+            return NoContent();
+        }
+    }
+}
+
---
+++ WebApplicationAPI/WebApplicationAPI/DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationAPI.DTO
+{
+    public class UserDTO
+    {
+        [Required]
+        public required int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public required string Email { get; set; }
+    }
+}
